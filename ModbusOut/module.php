3b411880bb14fb88bf7a4f4eed4c2102f5b05dca
Jochen Dony <jochen.dony@econvis.eu>
2015-11-01 20:55:20 +0100

    erster Testinhalt

--- ModbusOut/module.php
+++ ModbusOut/module.php
@@ -4,20 +4,25 @@
  
         // Der Konstruktor des Moduls
         // Überschreibt den Standard Kontruktor von IPS
-        public function __construct($InstanceID) {
+        //public function __construct($InstanceID) {
             // Diese Zeile nicht löschen
-            parent::__construct($InstanceID);
+        //    parent::__construct($InstanceID);
  
             // Selbsterstellter Code
-        }
+        //}
  
         // Überschreibt die interne IPS_Create($id) Funktion
-        public function Create() {
-            // Diese Zeile nicht löschen.
-            parent::Create();
- 
+        public function Create()
+        {
+        	//Never delete this line!
+        	parent::Create();
+        		
+        	$this->RegisterPropertyInteger("outputID", 0);
+        	$this->RegisterPropertyInteger("statusID", 0);
+        	$this->RegisterPropertyInteger("autoID", 0);
+
         }
- 
+        
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
             // Diese Zeile nicht löschen
